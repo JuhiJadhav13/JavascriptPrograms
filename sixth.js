200abef593d9a085c21560d5587e65549a348bb9
juhijadhav <jadhavjuhi13@gmail.com>
2024-02-28 15:50:07 +0530

    sixth commit

---
+++ sixth.js
@@ -0,0 +1,10 @@
+var userName = prompt("Please enter your name:");
+
+
+if (userName !== null && userName !== "") {
+  
+  alert("Hello, " + userName + "!");
+} else {
+  
+  alert("Hello Enter username!");
+}
